bde55c53316245c40ae197fcdaff1c3d363ab8aa
Nataša Radmilović <nata.radmilovic@gmail.com>
2025-09-24 16:56:59 +0200

    fixed homepage design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,10 +14,18 @@ function App() {
         <Route
           path="/"
           element={
-            <div style={{ textAlign: "center", marginTop: "20px" }}>
-              <h1>Welcome to Mood Mosaic App</h1>
-              <p>Go to <a href="/mosaic">mosaic</a> to see the mood board</p>
-                 <p>Or <a href="/add-mood">create a new mood</a></p>
+            <div className='homepage-div'>
+              <div className='logo-div'>
+              <img className='logo' src='mood-mosaic-logo-new.svg'/>
+              </div>
+              <div className='title-div'>
+              <h1 className='title'>Welcome to the Mood Mosaic App</h1>
+              <p className='text'>Mood Mosaic is a simple app for tracking your moods by date, turning them into a colorful visual mosaic to help you see and understand your emotional patterns over time.</p>
+              <div className='buttons'>
+              <button className='btn' href="/mosaic">View mosaic</button>
+              <button className='btn' href="/add-mood">Add mood</button>
+              </div>
+          </div>
             </div>
           }
         />
